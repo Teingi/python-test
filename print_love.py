5f9a20eb7dff17d06fc344d65140d6b3c7a9e4f8
Teingi <tangqing@ruc.edu.cn>
2018-02-15 17:19:59 +0800

    打印英文心形图案

---
+++ print_love.py
@@ -0,0 +1,5 @@
+import time
+words = input('Please input the words you want to say!:')
+for item in words.split():
+    print('\n'.join([''.join([(item[(x-y) % len(item)] if ((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3 <= 0 else ' ') for x in range(-30, 30)]) for y in range(12, -12, -1)]))
+    time.sleep(1.5);
