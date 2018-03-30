0639561aadef74a4c3745bd094ed3ed7e9894b97
Teingi <tangqing@ruc.edu.cn>
2018-03-31 00:39:15 +0800

    敏感词用*代替

---
+++ test_006.py
@@ -0,0 +1,45 @@
+#-*-coding=utf-8
+
+# Python小程序练习---敏感词替换成*星号
+'''
+user_input=input('请评论：')
+for filter_word in open('E:\\code\\python\\filtered_words.txt'):
+    fw=filter_word.rstrip()
+    if fw in user_input:
+        fw_len=len(fw)
+        user_input=user_input.replace(fw,'*'*fw_len)
+
+else:
+    print(user_input)
+
+    '''
+#! /usr/bin/env python
+#! -*- coding: utf-8 -*-
+
+__author__ = 'Sophie'
+
+import re
+
+if __name__ == '__main__':
+    file = open("E:\\code\\python\\filtered_words.txt","r")
+    list_key_word =[]
+    try:
+        list_key_word = file.readlines()
+    finally:
+        file.close()
+    for i in range(len(list_key_word)):
+        list_key_word[i]=list_key_word[i].strip(" \n")
+    while(True):
+        userinput = raw_input("Please input something.\n")
+        result = ""
+        for x in list_key_word:
+            #result = userinput.replace(x,"*")
+            result = re.sub(x,"*",userinput)
+            userinput = result
+        print(result)
+        continue_or_not = raw_input("Do you want continue? Please input y/n. Any other inputs will be taken as y.\n")
+        if continue_or_not.lower() == 'n':
+            break
+    
+
+
