8f61db90052c06f2da58b8b4b7ad6107b64c0f0e
Teingi <tangqing@ruc.edu.cn>
2018-04-18 23:26:14 +0800

    线程锁定

---
+++ thread_lock.py
@@ -0,0 +1,66 @@
+#---coding:utf-8----
+import time, threading
+
+#########################################################################
+"""
+如果多个线程共同对某个数据修改，则可能出现不可预料的结果，
+为了保证数据的正确性，需要对多个线程进行同步。
+
+我们定义了一个共享变量balance，初始值为0，并且启动两个线程，先存后取，理论上结果应该为0，但是，由于线程的调度是由操作系统决定的，
+当t1、t2交替执行时，只要循环次数足够多，balance的结果就不一定是0了。
+"""
+
+# 假定这是你的银行存款:
+balance = 0
+
+def change_it(n):
+    # 先存后取，结果应该为0:
+    global balance
+    balance = balance + n
+    balance = balance - n
+
+def run_thread(n):
+    for i in range(100000):
+        change_it(n)
+
+t1 = threading.Thread(target=run_thread, args=(5,))
+t2 = threading.Thread(target=run_thread, args=(8,))
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+print(balance)
+
+####################################################################
+#通过threading.Lock()来实现锁定线程执行过程
+'''
+
+import time, threading
+
+balance = 0
+lock = threading.Lock()
+
+def change_it(n):
+    # 先存后取，结果应该为0:
+    global balance
+    balance = balance + n
+    balance = balance - n
+
+def run_thread(n):
+    for i in range(100000):
+        # 先要获取锁:
+        lock.acquire()
+        try:
+            # 放心地改吧:
+            change_it(n)
+        finally:
+            # 改完了一定要释放锁:
+            lock.release()
+t1 = threading.Thread(target=run_thread, args=(5,))
+t2 = threading.Thread(target=run_thread, args=(8,))
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+print(balance)
+'''
