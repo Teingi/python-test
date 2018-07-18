63d7c191deae9f1cfedbc1de83e565b6ab034995
Teingi <tangqing@ruc.edu.cn>
2018-07-18 11:57:38 +0800

    二叉搜索树转换为累加树

---
+++ leetcode_538.py
@@ -0,0 +1,25 @@
+# leetcode 538. 把二叉搜索树转换为累加树
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def __init__(self):
+        self.lSum = 0
+    def convertBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        self.convertBST(root.right)
+        self.lSum += root.val
+        root.val = self.lSum
+        self.convertBST(root.left)
+        return root
