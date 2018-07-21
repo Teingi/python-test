0988c676fa581c4f7024f7a253060335f3215e09
Teingi <tangqing@ruc.edu.cn>
2018-07-22 00:13:18 +0800

    分糖果

---
+++ leetcode_575.py
@@ -0,0 +1,10 @@
+#leetcode 575. 分糖果
+
+
+class Solution(object):
+    def distributeCandies(self, candies):
+        """
+        :type candies: List[int]
+        :rtype: int
+        """
+        return min(len(candies)/2 , len(set(candies)))
