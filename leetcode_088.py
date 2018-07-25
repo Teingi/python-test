09e43de42b9f39fd66a03e86555bb33b8ddf6c79
Teingi <tangqing@ruc.edu.cn>
2018-07-26 00:45:26 +0800

    合并两个有序数组

---
+++ leetcode_088.py
@@ -0,0 +1,13 @@
+#leetcode 88. 合并两个有序数组
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[m:m+n]=nums2[:n]
+        nums1.sort()
