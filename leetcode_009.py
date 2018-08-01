370700ba1b3e3e00804c57efe8f9dd5d33a750ea
Teingi <tangqing@ruc.edu.cn>
2018-08-01 13:03:51 +0800

    判断回文数

---
+++ leetcode_009.py
@@ -0,0 +1,13 @@
+# leetcode 9. 回文数
+
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if str(x) == str(x)[::-1]:
+            return True
+        else:
+            return False
