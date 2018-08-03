ecf5d58bcdd36fa801ccf4590bb37a9277a87100
Teingi <tangqing@ruc.edu.cn>
2018-08-03 20:00:32 +0800

    判断环形链表

---
+++ leetcode_141.py
@@ -0,0 +1,15 @@
+# leetcode 141. 环形链表
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        fast = slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if slow == fast:
+                return True
+        return False
