6468cf87d45f8b4fa2d1e4794a0f7be2ab98d77c
Teingi <tangqing@ruc.edu.cn>
2018-08-07 14:18:02 +0800

    旋转数组

---
+++ leetcode_189.py
@@ -0,0 +1,12 @@
+#leetcode 189. 旋转数组
+
+
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        for i in range(k):
+            nums.insert(0,nums.pop())
