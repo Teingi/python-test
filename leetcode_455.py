5bd0b725e6080d975109ad01a78f330fb8750bdf
Teingi <tangqing@ruc.edu.cn>
2018-08-16 00:07:54 +0800

    分发饼干

---
+++ leetcode_455.py
@@ -0,0 +1,20 @@
+# leetcode 455. 分发饼干
+
+
+from collections import Counter
+class Solution(object):
+    def findContentChildren(self, children, cookies):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+        cookies.sort()
+        children.sort()
+        i = 0
+        for cookie in cookies:
+            if i >= len(children):
+                break
+            if children[i] <= cookie:
+                i += 1
+        return i
