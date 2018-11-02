b806981211fb73aa77dba45779b4f7ae4ef1ae11
Teingi <tangqing@ruc.edu.cn>
2018-11-02 22:18:39 +0800

    求二分查找

---
+++ leetcode_704.py
@@ -0,0 +1,20 @@
+#leetcode 704. 二分查找
+
+
+class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
