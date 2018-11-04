b0d2683c16521a3f05cebf1e454acfa928ef3228
Teingi <tangqing@ruc.edu.cn>
2018-11-04 10:30:07 +0800

    转换成小写字母

---
+++ leetcode_709.py
@@ -0,0 +1,10 @@
+# leetcode 709. 转换成小写字母
+
+
+class Solution(object):
+    def toLowerCase(self, str):
+        """
+        :type str: str
+        :rtype: str
+        """
+        return str.lower()
